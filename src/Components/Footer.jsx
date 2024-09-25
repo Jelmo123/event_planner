bbe6b0b73e4ba080cfb0651460c77869b895ff8e
Jelmo123 <j.w.golsteijn@outlook.com>
2024-09-25 07:18:01 -0400

    Finished lab

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <section className='footer'>
+                This is a section
+                <p>
+                    This is a paragraph
+                </p>
+                <div>
+                    This is a div
+                </div>
+            </section>
+        </footer>
+    );
+};
+
+export default Footer;
